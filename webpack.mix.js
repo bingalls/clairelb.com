d02d8f0ac09ab841d7876b34a12ba8b3bcbf4d25
Jason Beggs <jasonlbeggs@gmail.com>
2018-08-02 11:27:54 -0400

    Install TailwindCSS

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,7 +1,9 @@
 let mix = require('laravel-mix');
 let build = require('./tasks/build.js');
 
-// mix.disableSuccessNotifications();
+require('laravel-mix-tailwind');
+require('laravel-mix-purgecss');
+
 mix.setPublicPath('source/assets/build');
 mix.webpackConfig({
   plugins: [
@@ -19,7 +21,8 @@ mix.webpackConfig({
 mix
   .js('source/_assets/js/main.js', 'js')
   .postCss('source/_assets/css/main.css', 'css')
-  .options({
-    processCssUrls: false,
+  .tailwind()
+  .purgeCss({
+    folders: ['source'],
   })
   .version();
