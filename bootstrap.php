eec57009a00af916bee7e49f5c594227a005a5d9
Jason Beggs <jasonlbeggs@gmail.com>
2018-08-03 14:07:53 -0400

    Add a _redirects file for Netlify

--- bootstrap.php
+++ bootstrap.php
@@ -5,13 +5,10 @@ use TightenCo\Jigsaw\Jigsaw;
 /** @var $container \Illuminate\Container\Container */
 /** @var $events \TightenCo\Jigsaw\Events\EventBus */
 
-/**
- * You can run custom code at different stages of the build process by
- * listening to the 'beforeBuild', 'afterCollections', and 'afterBuild' events.
- *
- * For example:
- *
- * $events->beforeBuild(function (Jigsaw $jigsaw) {
- *     // Your code here
- * });
- */
+$events->afterBuild(function ($jigsaw) {
+    $redirects = file_get_contents('source/_redirects');
+
+    if ($redirects) {
+        file_put_contents('build_production/_redirects', $redirects);
+    }
+});
