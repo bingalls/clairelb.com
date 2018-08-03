f36f9dae41503be8f76d9d3ff83cff7ea890a98b
Jason Beggs <jasonlbeggs@gmail.com>
2018-08-03 12:14:58 -0400

    Add a 404 page

---
+++ source/404.blade.php
@@ -0,0 +1,11 @@
+@extends('_layouts.master')
+
+@section('content')
+    <div class="container text-center my-8">
+        <h1 class="text-center mb-2 text-5xl font-serif">404</h1>
+        <p class="mx-auto mb-8 text-center text-lg">
+            Oops! We couldn't find the page you're looking for...
+        </p>
+        <a href="/" class="text-grey-darkest hover:opacity-75 transition">Go back home</a>
+    </div>
+@endsection
--- source/assets/build/js/main.js
+++ source/assets/build/js/main.js
@@ -1,1 +1,99 @@
-!function(n){var e={};function t(r){if(e[r])return e[r].exports;var o=e[r]={i:r,l:!1,exports:{}};return n[r].call(o.exports,o,o.exports,t),o.l=!0,o.exports}t.m=n,t.c=e,t.d=function(n,e,r){t.o(n,e)||Object.defineProperty(n,e,{configurable:!1,enumerable:!0,get:r})},t.n=function(n){var e=n&&n.__esModule?function(){return n.default}:function(){return n};return t.d(e,"a",e),e},t.o=function(n,e){return Object.prototype.hasOwnProperty.call(n,e)},t.p="/",t(t.s=0)}({0:function(n,e,t){t("A7k7"),n.exports=t("Ymb9")},A7k7:function(n,e){var t=document.querySelector("#nav-button"),r=document.querySelector("#mobile-nav-links");t.addEventListener("click",function(n){if(r.classList.contains("hidden"))return r.classList.remove("hidden");r.classList.add("hidden")})},Ymb9:function(n,e){}});
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "/";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./source/_assets/css/main.css":
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ }),
+
+/***/ "./source/_assets/js/main.js":
+/***/ (function(module, exports) {
+
+var navButton = document.querySelector('#nav-button');
+var navLinks = document.querySelector('#mobile-nav-links');
+
+navButton.addEventListener('click', function (e) {
+  if (navLinks.classList.contains('hidden')) {
+    return navLinks.classList.remove('hidden');
+  }
+  navLinks.classList.add('hidden');
+});
+
+/***/ }),
+
+/***/ 0:
+/***/ (function(module, exports, __webpack_require__) {
+
+__webpack_require__("./source/_assets/js/main.js");
+module.exports = __webpack_require__("./source/_assets/css/main.css");
+
+
+/***/ })
+
+/******/ });
