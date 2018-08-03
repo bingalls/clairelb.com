ade5b6e5a698090830f74f0c9e19908b9623e79f
Jason Beggs <jasonlbeggs@gmail.com>
2018-08-02 20:23:46 -0400

    Change responsive widths

--- source/portfolio.blade.php
+++ source/portfolio.blade.php
@@ -4,18 +4,18 @@
 <div>
     <section class="container mx-auto mt-8">
         <h1 class="text-center mb-2 text-4xl md:text-5xl font-serif font-normal">Portfolio</h1>
-        <p class="md:w-3/4 mx-4 md:mx-auto mb-8 text-center">
+        <p class="md:w-2/3 mx-4 md:mx-auto mb-8 text-center">
             This portfolio displays a wide variety of the best Interior Design work Claire accomplished while at Athens Technical College.
         </p>
     </section>
     @foreach ($portfolio->sortBy('order') as $project)
         <div class="container flex flex-col justify-center px-4 sm:px-0 mb-16">
             <a
-                class="inline-block mx-auto group no-underline text-grey-darkest text-center"
+                class="inline-block md:w-2/3 mx-auto group no-underline text-grey-darkest text-center"
                 href="/portfolio/{{ $project->getFilename() }}"
             >
                 <img
-                    class="block w-full md:w-auto md:h-lg shadow-lg rounded mb-4 mx-4 md:mx-0 group-hover:lift transition"
+                    class="block w-full shadow-lg rounded mb-4 mx-4 md:mx-0 group-hover:lift transition"
                     src="/assets/images/portfolio/{{ $project->getFilename() }}/{{ $project->images[0] }}"
                 >
                 <h2 class="inline font-serif font-normal leading-tight text-xl md:text-3xl border-b border-transparent group-hover:border-grey-darkest transition">
